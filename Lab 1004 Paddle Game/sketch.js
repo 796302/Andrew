c0b98775e14901232bea6285dd39e0859ae2e5a1
796302 <42678131+796302@users.noreply.github.com>
2018-10-16 08:20:51 -0700

    Changed checkCollision function

--- Lab 1004 Paddle Game/sketch.js
+++ Lab 1004 Paddle Game/sketch.js
@@ -1,45 +1,42 @@
-//Global variable
-var paddle;
-var Balls = [];
-function setup(){
-//canvas
+//  Global variables
+var balls = [];
+var paddle = [];
+
+// seting up project
+function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(20, 20, 20);
-  loadBalls(12);
-//rectangle code - vvvvvvvvvvvvvvvvvvvvvvv
-//var loc = createVector(random(width), random(height));
-//var vel = createVector(random(-5,5), random(-5,5));
-//var col = color(random(0, 255), 0, 0)
-//paddle = new rectangle(loc, vel, col)
+  background(random(20,250), random(20,250), random(20,250)); // draws canvas
+  loadBalls(10);  // loads balls
+  paddle = new Paddle(createVector(0, 0), 33, color(random(255), random(255), random(255)))
 }
-//create Balls
-function loadBalls(numBalls){
-  for(var i = 0; i < numBalls; i++){
-      var loc = createVector(random(width), random(0,200));
-      var vel = createVector(random(-3,3), random(-3,3));
-      var rad = random(20,40);
-      var col = color(255, 255, 255);
-      var b = new Ball(loc, vel, rad, col);
-      Balls.push(b);
-
-}
-
-}
-
-function draw(){
-//  paddle.run();
-  for(var i = 0; i < Balls.length; i++){
-    Balls[i].run();
-//splice balls
-//    var x = Balls[i]
-//    var y = chaser.loc;
-//    var z = x.loc;
-//    var dist = y.dist(z);
-//    if(dist <= 25){
-//      Balls.splice(i , 1)
-//    }
+//
+function draw() {
+  background(0,0,0,);
+  paddle.run();
+  for(var i = 0; i < balls.length; i = i + 1){
+    balls[i].run()
   }
+  //getting rid of the boids
+  for (var i = 0; i <balls.length; i++){
+var w = paddle.width
+var h = paddle.height
+    if(){
+    balls.splice(i , 1)
+  }
+ }
+}
+//loading the balls
+function loadBalls(numBalls){
+//creating the balls
+    for(var i = 0; i < numBalls; i++){
+        var loc = createVector(random(width), random(100, 200));
+        var vel = createVector(random(-3,3), random(-3,3));
+        var radius = random(30,30);
+        var col = color(random(255), random(255), random(255));
+        var b = new Ball(loc, vel, radius, col);
+        balls.push(b);
 
-
+    }
+    console.log(balls);
 }

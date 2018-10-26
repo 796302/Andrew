b0454ac4bcf5408d7eec65ce9a7aafe622db2316
796302 <42678131+796302@users.noreply.github.com>
2018-10-26 08:35:47 -0700

    yuhhh

---
+++ Lab 1022 Bubble sort/bubble.js
@@ -0,0 +1,23 @@
+words = ['fortnite', 'bortnite', 'chugjug', 'johnwick', 'sit', 'ez', 'poggers', 'fortniteburger'];
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(0, 0, 0); // draws canvas
+
+for(var i = 0; i > words.length; i++){
+  words.push(random(words[i]))
+}
+
+for(var i = words.length - 1; i >=1; i--){
+  for(var j = 0; j < words.length; j++){
+    if(words[j] > words[j + 1]){
+      var temp = words[j];
+      words[j] = words[j + 1];
+      words[j + 1] = temp;
+      console.log(words);
+    }
+  }
+}
+
+
+}

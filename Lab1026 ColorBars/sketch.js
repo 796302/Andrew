2c02e6e922624dc8d1301658984d8dbf4473c54e
796302 <42678131+796302@users.noreply.github.com>
2018-10-30 08:49:22 -0700

    Do bubblesort/selectionsort/average colors

--- Lab1026 ColorBars/sketch.js
+++ Lab1026 ColorBars/sketch.js
@@ -24,12 +24,26 @@ for(var row = 0; row < numberOfSquares; row++) {
     var y = 100*col
     var loc = createVector(x, y);
     var c;
+
+    var r = random(0, 255);
+    var g = random(0, 255);
+    var b = random(0, 255);
     if((row + col) % 2){
-      c = color(random(0,255), random(0,255),random(0,255));
+      c = color(r, g, b);
+for(var i = squares.length - 1; i >= 1; i--){
+  for(var j = 0; j < squares.length; j++){
+    if(squares[j] > squares[j + 1]){
+      var temp = squares[j];
+      squares[j] = squares[j + 1];
+      squares[j + 1] = temp;
 
     }
+  }
+}
+    }
     else {
-      c = color(random(0,255), random(0,255),random(0,255));
+      c = color(r, g, b);
+
     }
     var s = new Sqr(loc, 100, c);
     squares.push(s);

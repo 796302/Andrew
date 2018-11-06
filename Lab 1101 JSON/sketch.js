f29e9f6cb55a2971c0521c75c266be23f4d2ca91
796302 <42678131+796302@users.noreply.github.com>
2018-11-06 08:43:12 -0800

    pop

--- Lab 1101 JSON/sketch.js
+++ Lab 1101 JSON/sketch.js
@@ -1,14 +1,12 @@
-//  Global variables
 var data;
-function preload(){
-  data = loadJSON("pokedexDataBase.json");
+function setup(){
+  loadJSON("population.json", gotData)
 }
+function gotData(jData){
+  data = jData;
+}
+function draw() {
+  if(data) {
 
-function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
-  background(random(20,250), random(20,250), random(20,250)); // draws canvas
-
-data.pokemon[1].name
-console.log();
+  }
 }

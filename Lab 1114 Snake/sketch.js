47c785f5d4b82a91439dee199e5421318d4c7556
796302 <42678131+796302@users.noreply.github.com>
2018-11-14 08:51:23 -0800

    yaeh

---
+++ Lab 1114 Snake/sketch.js
@@ -0,0 +1,8 @@
+var snake;
+var w = 30;
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(0, 0, 0); // draws canvas
+
+}
---
+++ Lab 1114 Snake/square.js
@@ -0,0 +1,21 @@
+function Snake(location, velocity, col){
+  this.loc = location;
+  this.vel = velocity;
+  this.col = col;
+
+}
+this.run = function(){
+  this.checkEdges();
+  this.update();
+  this.render();
+}
+this.update = function(){
+this.
+}
+this.checkEdges = function(){
+
+}
+this.render = function(){
+  fill(this.col);
+  rect(windowWidth/2, height/2, w, w)
+}

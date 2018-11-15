c8d3fd5cc407e1ef5feb820651836483e8a7027a
796302 <42678131+796302@users.noreply.github.com>
2018-11-15 08:50:36 -0800

    barbaba

--- Lab 1114 Snake/sketch.js
+++ Lab 1114 Snake/sketch.js
@@ -1,8 +1,39 @@
 var snake;
-var w = 30;
+var squares = [];
+var numberOfSquares = 40;
+var squareSize;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(0, 0, 0); // draws canvas
+  squareSize = width/numberOfSquares
+  loadSquares(20);
 
+for(var i = 0; i < squares.length; i++){
+  squares[i].render();
+  }
+}
+
+function loadSquares(){
+  //  create a square object and push it into the squares array
+var rowNum = 0;
+var colNum = 0;
+
+for(var row = 0; row < 40; row++) {
+  var x = 20*row
+  for(var col = 0; col < 40; col++) {
+    var y = 20*col
+    var loc = createVector(x, y);
+    var c;
+    if((row + col) % 2){
+      c = color(0, 0, 0);
+    }
+    else {
+      c = color(255, 255, 255);
+    }
+    var s = new Sqr(loc, 100, c);
+    squares.push(s);
+
+    }
+  }
 }
--- Lab 1114 Snake/square.js
+++ Lab 1114 Snake/square.js
@@ -1,21 +1,12 @@
-function Snake(location, velocity, col){
+function Sqr(location, sideLength, col){
   this.loc = location;
-  this.vel = velocity;
-  this.col = col;
+  this.w = sideLength
+  this.col = col
 
-}
-this.run = function(){
-  this.checkEdges();
-  this.update();
-  this.render();
-}
-this.update = function(){
-this.
-}
-this.checkEdges = function(){
 
-}
-this.render = function(){
+  this.render = function(){
   fill(this.col);
-  rect(windowWidth/2, height/2, w, w)
+  rect(this.loc.x, this.loc.y, this.w, this.w)
+  }
+
 }

78d30e9ff7e194cfeb5f685d219e5fe397c934da
796302 <42678131+796302@users.noreply.github.com>
2018-11-19 08:47:34 -0800

    urah

--- Lab 1114 Snake/sketch.js
+++ Lab 1114 Snake/sketch.js
@@ -1,44 +1,42 @@
-var snake = [];
-//snake bodies
-var squares = [];
-//movement for snake
-var numberOfSquares = 40;
-var squareSize;
-function setup() {
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
-  background(0, 0, 0); // draws canvas
-  squareSize = width/numberOfSquares
-  loadSquares(20);
+var w = 20;
+var cols, rows;
+var snake;
 
-for(var i = 0; i < squares.length; i++){
-  squares[i].render();
-  }
+function setup(){
+  var cnv = createCanvas(800, 800);
+  cnv.position(windowWidth-width)/(2, 30);
+  background(20, 20, 20);
+  cols = width/w;
+  rows = height/w;
+  var loc = createVector(width/2, height/2);
+  var vel = createVector(5, 0);
+  snake = new Snake(loc, vel);
 }
 
-function loadSquares(){
-  //  checkerboard for movement of snakes
-var rowNum = 0;
-//row for movement of snakes
-var colNum = 0;
-//column for movement of snakes
-//creates the amount of pixels the snake can move vvvvvv
-for(var row = 0; row < 40; row++) {
-  var x = 20*row
-  for(var col = 0; col < 40; col++) {
-    var y = 20*col
-    var loc = createVector(x, y);
-    var c;
-    if((row + col) % 2){
-      c = color(255, 255, 255);
-//keeps white background
-    }
-    else {
-      c = color(255, 255, 255);
-    }
-    var s = new Sqr(loc, 100, c);
-    squares.push(s);
+function draw(){
+  background(20, 20, 20, 6000);
+  snake.run();
+}
 
-    }
+function keyPressed(){
+  //up
+  if(keyCode === 38){
+    snake.vel = createVector(0, -20);
+    snake.loc.add(snake.vel);
+  }
+  //down
+  if(keyCode === 40){
+    snake.vel = createVector(0, 20);
+    snake.loc.add(snake.vel);
+  }
+  //right
+  if(keyCode === 39){
+    snake.vel = createVector(20, 0);
+    snake.loc.add(snake.vel);
+  }
+  //left
+  if(keyCode === 37){
+    snake.vel = createVector(-20, 0);
+    snake.loc.add(snake.vel);
   }
 }

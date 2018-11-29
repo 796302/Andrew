cb54bf2cf95ec41c3da005bf7446aad02a6d4f19
796302 <42678131+796302@users.noreply.github.com>
2018-11-29 08:51:05 -0800

    gagagave

--- Lab 1114 Snake/sketch.js
+++ Lab 1114 Snake/sketch.js
@@ -1,24 +1,23 @@
 var w = 20;
-var cols, rows;
-var snake = [];
+var snake;
 var food = [];
-var cols = Math.floor(800/20);
-var rows = Math.floor(800/20);
+var numSeg = 1;
+
 function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position(windowWidth-width)/(2, 30);
   background(20, 20, 20);
-  loadSnake(1);
+  loadSnake();
   loadFood(1);
 
-  frameRate(15);
+  frameRate(10);
 }
 
 function draw(){
   background(20, 20, 20, 6000);
-  for(var i = 0; i < snake.length; i = i + 1){
-  snake[i].run();
-}
+
+  snake.run();
+
 for(var i = 0; i < food.length; i = i + 1){
   food[i].run();
 }
@@ -26,56 +25,56 @@ for(var i = 0; i < food.length; i = i + 1){
 }
 function checkLoc(){
   for(var i = 0; i < food.length; i++){
-    var distX = food[i].loc.x - snake[0].loc.x;
-    var distY = food[i].loc.y - snake[0].loc.y;
+    var distX = food[i].loc.x - snake.loc.x;
+    var distY = food[i].loc.y - snake.loc.y;
     if(distX == (0) && distY == (0)){
       food.splice(i, 1);
       loadFood(1);
-      loadSnake(1);
+      snake.numSeg = snake.numSeg + 1;
     }
   }
 }
 
 
-
 function keyPressed(){
   //up
   if(keyCode === 38){
-    snake[0].vel = createVector(0, -20);
+    snake.vel = createVector(0, -20);
 
 
   }
   //down
   if(keyCode === 40){
-    snake[0].vel = createVector(0, 20);
+    snake.vel = createVector(0, 20);
 
   }
   //right
   if(keyCode === 39){
-    snake[0].vel = createVector(20, 0);
+    snake.vel = createVector(20, 0);
 
   }
   //left
   if(keyCode === 37){
-    snake[0].vel = createVector(-20, 0);
+    snake.vel = createVector(-20, 0);
 
   }
 }
-function loadSnake(numSnake){
-  for(var i = 0; i < numSnake; i++){
-
+function loadSnake(){
     var loc = createVector(width /2, height/2);
     var vel = createVector(0, 0);
     var col = color(0, 255, 0);
-    var s = new Snake(loc, vel, col);
-    snake.push(s);
+    snake = new Snake(loc, vel, col, numSeg);
+
   }
-}
+
+
+
+
 function loadFood(numFood){
   for(var i = 0; i < numFood; i++){
     var min = 1;
    //40 * 20 = 800
-   var max = 40;
+   var max = 39;
    var locX = (Math.floor(Math.random() * (max - min + 1) + min)) * 20;
    var locY = (Math.floor(Math.random() * (max - min + 1) + min)) * 20;
     var loc = createVector(locX, locY);
--- Lab 1114 Snake/snake.js
+++ Lab 1114 Snake/snake.js
@@ -2,21 +2,31 @@ function Snake(loc, vel){
 
   this.loc = loc;
   this.vel = vel;
-
+  this.numSeg = numSeg;
+   this.segments = [];
+   this.segments.push(createVector(0, 0));
   this.run = function(){
     this.update();
     this.render();
   }
 
   this.update = function(){
+    this.segments[0].x = this.loc.x;
+    this.segments[0].y = this.loc.y;
     this.loc.add(this.vel);
-    this.loc.x = constrain(this.loc.x, 0, width-w)
-    this.loc.y = constrain(this.loc.y, 0, width-w)
+    this.loc.x = constrain(this.loc.x, 0, 800-20)
+    this.loc.y = constrain(this.loc.y, 0, 800-20)
   }
-
   this.render = function(){
+    for(var i = 0; i < this.numSeg; i++){
+      noStroke();
+      fill(0, 255, 0);
+      this.segments.push(createVector(this.segments[i].x, this.segments[i].y));
+      rect(this.segments[i].x, this.segments[i].y, 20, 20)
+    }
+    noStroke();
     fill(0, 255, 0);
-    rect(this.loc.x, this.loc.y, w, w);
+    rect(this.loc.x, this.loc.y, 20, 20);
   }
 
 }

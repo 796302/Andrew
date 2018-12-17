d059fbccb3b38d663c30fade28229801995feb96
796302 <42678131+796302@users.noreply.github.com>
2018-12-17 07:35:49 -0800

    Final Game

--- Lab 1114 Snake/sketch.js
+++ Lab 1114 Snake/sketch.js
@@ -1,10 +1,17 @@
+//snake object
 var snake;
+//food all in the array
 var food = [];
+
 var numSeg = 1;
+//set up start screen
 var start = "true"
+//point system/number of segments
 var score = 0;
 function setup(){
+//speed of game
   frameRate(20);
+
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
@@ -17,17 +24,19 @@ function setup(){
 function draw(){
 
   background(20, 20, 20);
+  textSize(10)
+  text("score:" + score, 30, 30);
   snake.run();
-
+//repeat placement of food
   for(var i = 0; i < food.length; i++){
     food[i].run();
   }
-
+//call functions
   checkLoc();
   deadGame();
   gameStart();
 }
-
+//eat the food
 function checkLoc(){
   for(var i = 0; i < food.length; i++){
     var distX = food[i].loc.x - snake.loc.x;
@@ -37,22 +46,23 @@ function checkLoc(){
       loadFood(1);
       snake.segments.push(createVector(0, 0));
       console.log(snake.segments.length)
-      score = score + 1;
+      score++;
     }
   }
 }
-
+//creates snake
 function loadSnake(){
   var loc = createVector(200, 200);
   var vel = createVector(0, 0);
   snake = new Snake(loc, vel);
 }
-
+//creates a location for the food that goes into the food array
 function loadFood(numFood){
   for(var i = 0; i < numFood; i++){
     var min = 1;
     //40 * 20 = 800
     var max = 39;
+    //random x & y for 0-800
     var locX = (Math.floor(Math.random() * (max - min + 1) + min)) * 20;
     var locY = (Math.floor(Math.random() * (max - min + 1) + min)) * 20;
     var loc = createVector(locX, locY);
@@ -60,23 +70,27 @@ function loadFood(numFood){
     food.push(f);
   }
 }
-
+//movement for keys when pressed
 function keyPressed(){
    start = "false"
+//up arrow
   if(keyCode === 38){
     snake.vel = createVector(0, -20)
   }
+//down arrow
   if(keyCode === 40){
     snake.vel = createVector(0, 20)
   }
+//right arrow
   if(keyCode === 39){
     snake.vel = createVector(20, 0)
   }
+//left arrow
   if(keyCode === 37){
     snake.vel = createVector(-20, 0)
   }
 }
-
+//if dead then show end screen
 function deadGame(){
   if(snake.status == "true"){
     textFont()
@@ -88,7 +102,7 @@ function deadGame(){
       text("You Lose", 400, 350)
       text("Press w to restart", 400, 425)
 
-
+//w to restart game
     if(keyCode === 87){
       snake = 0
       loadSnake();
@@ -98,7 +112,7 @@ function deadGame(){
     }
   }
 }
-
+//when first started - show start screen
 function gameStart(){
   if(start == "true"){
     textFont()
